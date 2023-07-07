cdfee0628688be05e577c4d529a5799b62e3ee8a
abdelrhman <abdelrhman225328@bue.edu.eg>
2023-07-07 22:22:20 +0300

    Initial commit

---
+++ ShopemeWebParent/ShopemeBackend/src/main/java/com/shopeme/admin/MainController.java
@@ -0,0 +1,12 @@
+package com.shopeme.admin;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String getHomeView(){
+        return "index";
+    }
+}
---
+++ ShopemeWebParent/ShopemeBackend/src/main/java/com/shopeme/admin/ShopemeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopeme.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopemeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopemeBackendApplication.class, args);
+    }
+
+}
